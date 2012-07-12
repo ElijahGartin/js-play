ede6959f9347a428030ca7ce7abadaa1d9e18803
Elijah Gartin <elijah.gartin@gmail.com>
2012-07-11 21:57:40 -0600

    External Javascript File
    Buh... duh?

---
+++ js-play.js
@@ -0,0 +1,28 @@
+  <script type"text/javascript">
+    function makeMoney()
+    {
+      document.getElementById("money").innerHTML+="$ ";
+    }
+    function changeLeftButtonLabel()
+    {
+      document.getElementById("leftButton").innerHTML="You got me! ";
+    }
+
+    function changeRightButtonLabel()
+    {
+      document.getElementById("rightButton").innerHTML="You got me! ";
+    }
+
+    function reloadPage()
+    {
+      location.reload(true);
+    }
+
+    function fuckUser()
+    {
+      while(true)
+      {
+        document.write("!meow!");
+      }
+    }
+  </script>
